f1186a59caa868e2308e96425df7ab19b41d3576
sifan <sifan007sifu@gmail.com>
2023-10-13 17:56:55 +0530

    setting old model

--- backend/settings.py
+++ backend/settings.py
@@ -210,8 +210,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',    
 ]
 
-CELERY_BROKER_URL = 'redis://localhost:6379/1' 
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
+CELERY_BROKER_URL = 'redis://localhost:6379/0' 
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
