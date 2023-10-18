f6edaab2d37b954129328e7860dfccfbcc00b976
sifan <sifan007sifu@gmail.com>
2023-10-18 21:23:38 +0530

    redis post setting in the env

--- backend/settings.py
+++ backend/settings.py
@@ -240,7 +240,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [(config('redis'), 6379)],
+            "hosts": [('localhost', 6379)],
         },
     },
 }
